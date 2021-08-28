dcc6eb1fa86dd64769d07b6a6da9fc3503c36395
motoloto <motoloto0329@gmail.com>
2021-08-28 13:59:47 +0800

    First version initail. Support TWD to USD and USD to TWD

---
+++ background.js
@@ -0,0 +1,96 @@
+// background.js
+
+let color = '#3aa757';
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);
+});
+
+// right-click menu
+chrome.contextMenus.create(
+  { id: "rootMenu", title: "將所選金額： %s", contexts: ["selection"] },
+  function () {
+    if (chrome.extension.lastError) {
+      console.log("Got expected error: " + chrome.extension.lastError.message);
+    }
+  });
+
+function getExchangeRateFromStore(currencyMap) {
+  return new Promise(resolve => {
+    chrome.storage.local.get("exchangeRates", function (result) {
+      // console.log('Value set is ' + JSON.stringify(result));
+      resolve(result.exchangeRates[`${currencyMap}`]);
+    });
+  })
+}
+
+function makeExchange(rateKey,info) {
+  // const rateToUSD = getExchangeRateFromStore(`USD${rateKey[0]}`);
+  // const rateToTarget = getExchangeRateFromStore(`USD${rateKey[1]}`);
+  Promise.all([getExchangeRateFromStore(`USD${rateKey[0]}`), getExchangeRateFromStore(`USD${rateKey[1]}`)]).then(values => {
+    console.log(values[0] ,"+",values[1]);
+    const rateToUSD = values[0].Exrate;
+    const rateToTarget = values[1].Exrate;
+    const amount = Number(info.selectionText) / rateToUSD * rateToTarget;
+    if (!isNaN(amount)) {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { "amount": amount, "targetCcy":rateKey[1] }, function (response) {
+          console.log(response);
+        });
+      });
+    }
+  });
+}
+chrome.contextMenus.create(
+  { id: "USDtoTWD", title: "美金轉台幣", type: "normal", parentId: "rootMenu", contexts: ["selection"] });
+
+
+chrome.contextMenus.create(
+  { id: "TWDtoUSD", title: "台幣轉美金", type: "normal", parentId: "rootMenu", contexts: ["selection"] });
+
+chrome.contextMenus.onClicked.addListener(
+  (info, tab) => {
+    let rate = null;
+    console.log("info",info);
+    console.log("tab",tab);
+    if (info.menuItemId == 'USDtoTWD') {
+      rateKey = ["USD","TWD"];
+    } else if (info.menuItemId == 'TWDtoUSD') {
+      rateKey = ["TWD","USD"];
+
+    }
+    makeExchange(rateKey, info)
+  }
+
+);
+
+
+let exchangeRates = {}
+fetch('https://tw.rter.info/capi.php', {
+  method: 'GET', // *GET, POST, PUT, DELETE, etc.
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  },
+  credentials: 'same-origin',
+  redirect: 'follow', // manual, *follow, error
+  referrer: 'no-referrer', // *client, no-referrer
+}).catch(error => console.error('Error:', error))
+  .then(response => response.json())
+  .then(text => {
+    // console.log("exchangeRates", text);
+
+    chrome.storage.local.set({ "exchangeRates": text },
+      function () {
+        if (chrome.extension.lastError) {
+          console.log("Got expected error: " + chrome.extension.lastError.message);
+        }
+      });
+    // console.log('rate is ready:', text)
+    chrome.storage.local.get("exchangeRates", function (result) {
+      // console.log('Value set is ' + JSON.stringify(result));
+    });
+
+  });
+
---
+++ content.js
@@ -0,0 +1,78 @@
+function removeElement(elementId){
+  var elem = document.getElementById(elementId);
+  if(elem) elem.remove();
+}
+
+const elementId = "quick_cur";
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  removeElement(elementId);
+  console.log(request.amount);
+  const popText = `${request.targetCcy} $:  ${request.amount}`;
+  // alert("金額為" + request.amount);
+  // Callback for that request
+  // markSelection();
+  (function (popText) {
+    var markerTextChar = "\ufeff";
+    var markerTextCharEntity = "&#xfeff;";
+
+    var markerEl, markerId = elementId;
+
+    var selectionEl;
+
+
+    var doc = window.document;
+    var sel, range;
+    // Branch for IE <= 8 
+    if (doc.selection && doc.selection.createRange) {
+      // Clone the TextRange and collapse
+      range = doc.selection.createRange().duplicate();
+      range.collapse(false);
+
+      // Create the marker element containing a single invisible character by creating literal HTML and insert it
+      range.pasteHTML('<span id="' + markerId + '" style="position: relative;">' + markerTextCharEntity + '</span>');
+      markerEl = doc.getElementById(markerId);
+    } else if (window.getSelection) {
+      sel = window.getSelection();
+      range = sel.getRangeAt(0).cloneRange();
+      range.collapse(false);
+
+      // Create the marker element containing a single invisible character using DOM methods and insert it
+      markerEl = doc.createElement("span");
+      markerEl.id = markerId;
+      markerEl.appendChild(doc.createTextNode(markerTextChar));
+      range.insertNode(markerEl);
+    }
+
+    if (markerEl) {
+      // Lazily create element to be placed next to the selection
+      if (!selectionEl) {
+        selectionEl = doc.createElement("div");
+        selectionEl.id = elementId;
+        selectionEl.style.border = "solid darkblue 1px";
+        selectionEl.style.backgroundColor = "lightgoldenrodyellow";
+        selectionEl.innerHTML = `${popText}`;
+        selectionEl.style.position = "absolute";
+
+        doc.body.appendChild(selectionEl);
+      }
+
+      // Find markerEl position http://www.quirksmode.org/js/findpos.html
+      var obj = markerEl;
+      var left = 0, top = 0;
+      do {
+        left += obj.offsetLeft;
+        top += obj.offsetTop;
+      } while (obj = obj.offsetParent);
+
+      // Move the button into place.
+      // Substitute your jQuery stuff in here
+      selectionEl.style.left = left + "px";
+      selectionEl.style.top = top + "px";
+
+      markerEl.parentNode.removeChild(markerEl);
+    }
+
+  })(popText);
+  sendResponse({ message: 'Background has received that message ?' });
+});
